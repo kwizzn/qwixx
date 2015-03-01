02e5cc97bb4be1c20235a32f0f50c85a369e4834
Christian Neuhäuser <chris@type10.com>
2015-03-01 13:54:55 +0100

    Adds a test

--- test/ColorModel.js
+++ test/ColorModel.js
@@ -1,10 +1,27 @@
 'use strict';
-var ColorModel = require('../lib/models/ColorModel');
+var _ = require('underscore'),
+    ColorModel = require('../lib/models/ColorModel');
 
 describe('ColorModel', function() {
 
     beforeEach(function() { });
-    afterEach(function() { });
+
+    afterEach(function() {
+        window.localStorage.clear();
+    });
+
+    describe('initialize()', function() {
+        it('should restore correctly from local storage', function(){
+            var defaults = _(ColorModel.prototype.defaults).clone();
+            defaults[2] = defaults[4] = defaults[6] = true;
+            window.localStorage.setItem('red', JSON.stringify(defaults));
+
+            var colorModel = new ColorModel(null, { color: 'red' });
+            expect(colorModel.get(2)).to.be.true;
+            expect(colorModel.get(4)).to.be.true;
+            expect(colorModel.get(6)).to.be.true;
+        });
+    });
 
     describe('getItemsCount()', function() {
         it('should return 0 after initialization', function(){
